f6ad860bcb102db7f640b794323820e90ec1cc96
Master-Faraz <farazali9028@gmail.com>
2021-09-25 19:48:13 +0530

    code

---
+++ C - Code/1. Array/tempCodeRunnerFile.c
@@ -0,0 +1,12 @@
+        // for (int j = 0; j < size; j++)
+        // {
+        //     if (ptr[j] == ptr[i])
+        //     {
+        //         // int n = size - j;
+        //         // while (n--)
+        //         // {
+        //             ptr[j] = ptr[j + 1];
+        //             counter++;
+        //         // }
+        //     }
+        // }
